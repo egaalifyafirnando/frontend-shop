d6a68e0878f0c6a805755e5e88bffae014a6bbaf
Ega Alifya Firnando <egaalifyaf@gmail.com>
2022-09-13 20:41:32 +0700

    setup: mixins

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// DEFINE MIXINS FOR GLOBAL FUNCTION
+app.mixin({
+    methods: {
+        // MONEY FORMAT THOUSANDS
+        moneyFormat(number) {
+            let reverse = number.toString().split('').reverse().join(''),
+                thousands = reverse.match(/\d{1,3}/g);
+
+            thousands = thousands.join('.').split('').reverse().join('');
+            return thousands;
+        },
+
+        // CALCULATE DISCOUNT
+        calculateDiscount(product) {
+            return product.price - (product.price * product.discount) / 100;
+        },
+    },
+});
+
+app.mount('#app');
